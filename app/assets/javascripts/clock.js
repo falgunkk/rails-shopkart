f7cfc9a2bf880626864cb7a5035ac18e39c3af76
falgun <falgun@qburst.com>
2016-10-12 20:05:51 +0530

    deal of the day

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,15 +1,27 @@
-function time() {
- var today = new Date();
- var h = today.getHours();
- var m = today.getMinutes();
- var s =today.getSeconds();
- m=checkTime(m);
- s=checkTime(s);
- today=today.toDateString();
- document.getElementById('time').innerHTML = today + "&emsp; " + h + ":" + m + ":" + s + "&emsp;";
- var t=setTimeout(time,500);
-}
-function checkTime(i) {
- if (i<10) {i = "0" + i};
- return i;
-}
+
+// <script>
+// var h1 = document.getElementById('clock'),
+//     seconds = 0, minutes = 0, hours = 0,
+//     t;
+
+// function add() {
+//     seconds++;
+//     if (seconds >= 60) {
+//         seconds = 0;
+//         minutes++;
+//         if (minutes >= 60) 
+//         {
+//             minutes = 0;
+//             hours++;
+//         }
+//     }
+    
+//     h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+//     timer();
+// }
+// function timer() {
+//     t = setTimeout(add, 1000);
+// }
+// timer();
+// </script>
